7ddc074bc1c4943b603e57e57f01bea87d7fa324
Victor Nguyen <tqnguyen2073@gmail.com>
2025-08-14 15:04:11 +0700

    chore: clean warning

--- src/main/java/com/pvt/admin/AdminApplication.java
+++ src/main/java/com/pvt/admin/AdminApplication.java
@@ -3,8 +3,6 @@ package com.pvt.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.awt.*;
-import java.net.URI;
 
 @SpringBootApplication
 public class AdminApplication {
